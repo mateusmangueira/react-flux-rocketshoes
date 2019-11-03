27ef593029783a34d6b51a5bc23199c41cf7df91
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-03 17:19:37 -0300

    Estilizacao de paginas completa

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div>Carrinho</div>
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QUANTIDADE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src='https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe2.jpg?ims=326x'
+                alt='Tenis' />
+            </td>
+            <td>
+              <strong>TENIS DOIDAO</strong>
+              <span>R$129,00</span>
+            </td>
+            <td>
+              <div>
+                <button type='button'>
+                  <MdRemoveCircleOutline size={20} color='#7159c1' />
+                </button>
+                <input type='number' readOnly value={2} />
+                <button type='button'>
+                  <MdAddCircleOutline size={20} color='#7159c1' />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,00</strong>
+            </td>
+            <td>
+              <button type='button'>
+                <MdDelete size={20} color='#7159c1' />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type='button'>Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$590,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 
 }
