1847fe291e1fc0da7522967387a7340815a2943f
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-03 16:17:46 -0300

    Iniciando a estilizar a pagina Home

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Cart() {
-  return (
-    <h1>OI Redux Carrinho</h1>
-  );
+  return <div>Carrinho</div>
+
 }
