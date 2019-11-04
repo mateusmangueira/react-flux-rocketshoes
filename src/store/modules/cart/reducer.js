400395f79777c7c0844328f23fe385e05fb9a546
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-03 23:56:27 -0300

    Listagem de produtos no carrinho iniciada, falta organizar produtos duplicados, remover e mudar quantidades dos produtos

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,7 +4,13 @@ export default function cart(state = [], action) {
   // Todos reducer ouvem todas as actions, por isso precisa o switch case para selecionar qual sera disparada pelo Type
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return [
+        ...state,
+        {
+          ...action.product,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }
