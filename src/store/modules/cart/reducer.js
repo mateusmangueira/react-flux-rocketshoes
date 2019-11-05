5b531390691e6111806084c4809f1aa9ebb7e1ac
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-05 18:15:24 -0300

    Ajeitando produtos duplicados com o Immer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,16 +1,24 @@
-// Todo reducer precisa iniciar o state, nesse caso iniciamos com o array vazio, pois o carrinho comeca zerado
+import produce from 'immer';
 
+// Todo reducer precisa iniciar o state, nesse caso iniciamos com o array vazio, pois o carrinho comeca zerado
 export default function cart(state = [], action) {
   // Todos reducer ouvem todas as actions, por isso precisa o switch case para selecionar qual sera disparada pelo Type
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return produce(state, draftState => {
+        const productIndex = draftState.findIndex(
+          p => p.id === action.product.id
+        );
+        if (productIndex >= 0) {
+          draftState[productIndex].amount += 1;
+        } else {
+          draftState.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
+
     default:
       return state;
   }
